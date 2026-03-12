17f4fbca434073b77ab25bc869916157f1a2f4f2
Adrian Briano <adrian.briano.garcia@outlook.com>
2026-03-12 17:08:01 -0600

    Update Application.java

--- src/main/java/com/linkedin/collections/Application.java
+++ src/main/java/com/linkedin/collections/Application.java
@@ -9,13 +9,15 @@ public class Application {
 
 		Room piccadilly = new Room("Piccadilly", "Guest Room", 3, 125.00);
 		Room oxford = new Room("Oxford", "Suite", 5, 225.0);
-
 		Guest john = new Guest("John", "Doe", false);
 		Guest maria = new Guest("Maria", "Doe", true);
 
 		Map<Room, Guest> roomGuestMap = new HashMap<>();
 		roomGuestMap.put(oxford, maria);
 		roomGuestMap.put(piccadilly, john);
+
+		Guest guest =  roomGuestMap.put(piccadilly, roomGuestMap.remove(oxford));
+		roomGuestMap.putIfAbsent(oxford, guest);
 
 		System.out.println("Oxford: " + roomGuestMap.get(new Room("Oxford", "Suite", 5, 225.0)	));
 		System.out.println("Piccadilly: " + roomGuestMap.get(piccadilly));
