745dc3cbfc057ef1465b9b27e0124ebd4d115261
Adrian Briano <adrian.briano.garcia@outlook.com>
2026-03-12 15:15:01 -0600

    Update Application.java

--- src/main/java/com/linkedin/collections/Application.java
+++ src/main/java/com/linkedin/collections/Application.java
@@ -1,13 +1,17 @@
 package com.linkedin.collections;
 
-import java.util.Arrays;
-import java.util.List;
+import java.util.*;
 
 public class Application {
 
 	public static void main(String[] args) {
 		
 		List<Integer> numbers = Arrays.asList(500, 1500, 2500, 1000, 3000, 2000);
-		
+
+        NavigableSet<Integer> treeSet = new TreeSet<>(numbers);
+
+		treeSet.subSet(1750,2750).forEach(System.out::println);
+
+		System.out.println(treeSet.ceiling(1750));
 	}
 }
