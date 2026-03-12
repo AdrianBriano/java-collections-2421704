6be429eaf98feaaee3f258f4267bbd22b317fd34
Adrian Briano <adrian.briano.garcia@outlook.com>
2026-03-12 17:05:39 -0600

    Update Application.java

--- src/main/java/com/linkedin/collections/Application.java
+++ src/main/java/com/linkedin/collections/Application.java
@@ -1,5 +1,8 @@
 package com.linkedin.collections;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class Application {
 
 	public static void main(String[] args) {
@@ -10,8 +13,12 @@ public class Application {
 		Guest john = new Guest("John", "Doe", false);
 		Guest maria = new Guest("Maria", "Doe", true);
 
-		
-				
+		Map<Room, Guest> roomGuestMap = new HashMap<>();
+		roomGuestMap.put(oxford, maria);
+		roomGuestMap.put(piccadilly, john);
+
+		System.out.println("Oxford: " + roomGuestMap.get(new Room("Oxford", "Suite", 5, 225.0)	));
+		System.out.println("Piccadilly: " + roomGuestMap.get(piccadilly));
 	}
 
 }
